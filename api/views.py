e33eb21cd00b866c4c3e74931245d895b2a7e5af
DEV-MatheusDiniz <mamakerrykinh@gmail.com>
2023-03-09 09:27:41 -0300

    alteração das views e urls do faturamentos e faturamentosItem, para que possa ter mais controle na edição da response

--- api/views.py
+++ api/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Faturamento, FaturamentoItem
 from .serializer import FaturamentoSerializer, FaturamentoItemSerializer
 
 
-class FaturamentoViewSet(viewsets.ModelViewSet):
-    queryset = Faturamento.objects.filter(st_situacao = 'previa').order_by('-dt_cadastro')
-    serializer_class = FaturamentoSerializer
+# Faturamento
+class FaturamentoAPIView(APIView):
 
-class FaturamentoItemViewSet(viewsets.ModelViewSet):
-    queryset = FaturamentoItem.objects.filter().order_by('-id')
-    serializer_class = FaturamentoItemSerializer
+    def get(self, request):
+        faturamentos = Faturamento.objects.filter(st_situacao = 'previa').order_by('-dt_cadastro')
+        serializer = FaturamentoSerializer(faturamentos, many=True)
+        return Response(serializer.data)
+
+
+# FaturamentoItem
+class FaturamentoItemAPIView(APIView):
+
+    def get(self, reequest):
+        faturamentoItem = FaturamentoItem.objects.all().order_by('-id')
+        serializer = FaturamentoItemSerializer(faturamentoItem, many=True)
+        return Response(serializer.data)
