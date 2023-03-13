9848d964f89aed0723d66bb4c6cf726923420771
DEV-MatheusDiniz <mamakerrykinh@gmail.com>
2023-03-13 20:05:23 -0300

    retirando importação da biblioteca dateutil

--- api/views.py
+++ api/views.py
@@ -1,12 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from pprint import pprint
-
 import locale
-
 from datetime import datetime
-from dateutil.parser import parse
 
 from .models import Faturamento
 from .models import FaturamentoItem
